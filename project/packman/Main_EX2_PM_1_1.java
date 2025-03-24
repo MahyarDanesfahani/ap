29ca499a96e5efdfb79830a8237284dc54ed655c
mahyar <mahyardanesfahani@gmail.com>
2025-03-24 16:29:46 +0330

    EX2_PM_1_1

--- project/packman/Main_EX2_PM_1_1.java
+++ project/packman/Main_EX2_PM_1_1.java
@@ -1,4 +1,21 @@
 package project.packman;
 
+import java.util.Scanner;
+
 public class Main_EX2_PM_1_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("please enter the number ? ");
+        int k = scanner.nextInt();
+        for (int i = 0; i < k + 2; i++) {
+            for (int j = 0; j < k + 2; j++) {
+                if (i == 0 || i == k + 1 || j == 0 || j == k + 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
