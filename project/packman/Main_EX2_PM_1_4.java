c1501d279d0634e4af410ee5eb7780354699d803
mahyar <mahyardanesfahani@gmail.com>
2025-03-25 14:30:31 +0330

    EX2_PM_2_1 and edit EX2_PM_1_4

--- project/packman/Main_EX2_PM_1_4.java
+++ project/packman/Main_EX2_PM_1_4.java
@@ -3,50 +3,51 @@ package project.packman;
 import java.util.Scanner;
 
 public class Main_EX2_PM_1_4 {
-
-    public static void packman_Direction (char[][] star , int row, int col , char s) {
-        Scanner scanner =  new Scanner(System.in);
+    public static boolean packman_Direction(char[][] star, int[] positionOfx, char s, Scanner scanner) {
         System.out.println("The direction of game\n" +
                 "char of 'W' = up\n" +
                 "char of 'A' = left\n" +
                 "char of 'D' = right\n" +
-                "char of 'S' = down");
+                "char of 'S' = down\n" +
+                "char of '0' = Exit");
+
         while (true) {
+            star[positionOfx[0]][positionOfx[1]] = ' ';
             System.out.print("Enter direction : ");
             String answer = scanner.next();
             if (answer.equals("0")) {
-                System.out.println("Exiting game...");
-                break;
+                System.out.println("Exiting game!!!!");
+                return true;
             }
-            star[row][col] = ' ';
-            switch (answer) {
+            int newRow = positionOfx[0];
+            int newCol = positionOfx[1];
+            switch (answer.toLowerCase()) {
                 case "w":
-                    if (row > 1){
-                        row--;
-                    }
+                    newRow--;
                     break;
                 case "s":
-                    if (row < star.length - 2){
-                        row++;
-                    }
+                    newRow++;
                     break;
                 case "a":
-                    if (col > 1){
-                        col--;
-                    }
+                    newCol--;
                     break;
                 case "d":
-                    if (col < star[0].length - 2){
-                        col++;
-                    }
+                    newCol++;
                     break;
                 default:
-                    System.out.println("Warning!! invalid input! Please enter W, A, S, D or 0.");
+                    System.out.println("Warning!! Invalid input!!! Please enter W, A, S, D or 0 .");
+                    continue;
             }
-            star[row][col] = s;
+
+            if (star[newRow][newCol] == '*') {
+                System.out.println("Hitting the game wall!!!! Game Over!!!");
+                System.exit(0);
+            }
+            positionOfx[0] = newRow;
+            positionOfx[1] = newCol;
+            star[positionOfx[0]][positionOfx[1]] = s;
 
             Main_EX2_PM_1_2.print(star);
         }
-        scanner.close();
     }
 }
--- project/test.java
+++ project/test.java
@@ -1,4 +1,8 @@
 package project;
 
+import project.packman.Main_EX2_PM_1_2;
+
 public class test {
+
 }
+
