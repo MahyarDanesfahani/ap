686f3e94bf8fbcde3fcc9c198bc8672e00df4fa6
mahyar <mahyardanesfahani@gmail.com>
2025-04-27 16:46:18 +0330

    E3_6

--- excercise/ex4/Light.java
+++ excercise/ex4/Light.java
@@ -1,5 +1,5 @@
 package excercise.ex4;
-
+/*
 public class Light {
     private int switch1; // 0 for down, 1 for up
     private int switch2; // 0 for down, 1 for up
@@ -35,6 +35,44 @@ public class Light {
 
     private void updateLampState() {
         lampState = (switch1 != switch2) ? 1 : 0;
+    }
+}
+*/
+public class Light {
+    private int[] switches;
+    private int lampState;
+
+    public Light() {
+        switches = new int[2];
+        switches[0] = 0;
+        switches[1] = 0;
+        updateLampState();
+    }
+
+    public int getSwitchState(int switchNum) {
+        if (switchNum == 1 || switchNum == 2) {
+            return switches[switchNum - 1];
+        } else {
+            throw new IllegalArgumentException("Switch number must be 1 or 2 .");
+        }
+    }
+
+    public int getLampState() {
+        return lampState;
+    }
+
+    public void toggleSwitch(int switchNum) {
+        if (switchNum == 1 || switchNum == 2) {
+            int index = switchNum - 1;
+            switches[index] = (switches[index] == 0) ? 1 : 0;
+            updateLampState();
+        } else {
+            throw new IllegalArgumentException("Switch number must be 1 or 2 .");
+        }
+    }
+
+    private void updateLampState() {
+        lampState = (switches[0] != switches[1]) ? 1 : 0;
     }
 }
 
